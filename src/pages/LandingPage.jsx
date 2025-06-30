4e23b26c732cca3677160d0597b875924dccc254
Ahmad Raza <161349444+382ahmadraza@users.noreply.github.com>
2025-06-30 23:09:24 +0500

    Updated LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -137,7 +137,7 @@ const HeroSlider = () => {
                           Start Your Journey
                           <ArrowRight className="inline ml-2 w-5 h-5" />
                         </button>
-                        <a href='https://youtu.be/TQoJ5EpI8gk?si=hXZPeJSnQFutukMG'>
+                        <a href='https://www.youtube.com/watch?v=TQoJ5EpI8gk'>
                           <button className="border-2 border-white text-white px-8 py-4 font-semibold text-lg hover:bg-white hover:text-gray-900 transition-all duration-300 flex items-center justify-center">
                           <Play className="w-5 h-5 mr-2" />
                           Watch Demo
